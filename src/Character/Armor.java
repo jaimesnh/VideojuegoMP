33646fde312cca8fd3c1e575292a6e52d58e07b7
dianalopezlozano <d.lopezlo.2023@alumnos.urjc.es>
2025-04-10 12:19:24 +0200

    deded

--- src/Character/Armor.java
+++ src/Character/Armor.java
@@ -1,4 +1,29 @@
 package Character;
 
-public class Armor {
+public class Armor implements Equipment {
+    private String name;
+    private int defense;
+
+    public Armor(String name, int defense) {
+        if (defense < 1 || defense > 3) {
+            throw new IllegalArgumentException("Defense value must be between 1 and 3");
+        }
+        this.name = name;
+        this.defense = defense;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getAttack() {
+        return 0;
+    }
+
+
+    public int getDefense() {
+        return defense;
+    }
 }
--- src/Character/Weapon.java
+++ src/Character/Weapon.java
@@ -1,5 +1,35 @@
 package Character;
 
-public class Weapon {
+public class Weapon implements Equipment {
+    private String name;
+    private int attack;
+    private boolean oneHand;
 
+    public Weapon(String name, int attack, boolean oneHand) {
+        if (attack < 1 || attack > 3) {
+            throw new IllegalArgumentException("Attack value must be between 1 and 3");
+        }
+        this.name = name;
+        this.attack = attack;
+        this.oneHand = oneHand;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getAttack() {
+        return attack;
+    }
+
+
+    public int getDefense() {
+        return 0;
+    }
+
+    public boolean isOneHand() {
+        return oneHand;
+    }
 }
