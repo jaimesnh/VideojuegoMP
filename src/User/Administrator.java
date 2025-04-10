849b76edb9885e53193e95418beefdbe0a813d81
“Turo” <arturo.vinuesadcg@gmail.com>
2025-04-10 11:15:57 +0200

    AS

--- src/User/Administrator.java
+++ src/User/Administrator.java
@@ -1,4 +1,35 @@
 package User;
 
 public class Administrator {
-}
+        public Administrador(String nombre, String nick, String password) {
+            super(nombre, nick, password);
+        }
+
+        public void editarPersonaje() {
+        }
+
+        public void añadirArma() {
+        }
+
+        public void añadirArmaduras() {
+        }
+
+        public void gestionarFortalezas() {
+        }
+
+        public void gestionarDebilidades() {
+        }
+
+        public void añadirEsbirros() {
+        }
+
+        public void validarDesafío() {
+        }
+
+        public void bloquearUsuario() {
+        }
+
+        public void desbloquearUsuario() {
+        }
+    }
+
--- src/User/Player.java
+++ src/User/Player.java
@@ -1,4 +1,62 @@
 package User;
 
 public class Player {
-}
+        private int numeroDeRegistro;
+        private Personaje personaje;
+
+        public player(String nombre, String nick, String password, int numeroDeRegistro) {
+            super(nombre, nick, password);
+            this.numeroDeRegistro = numeroDeRegistro;
+        }
+
+        public void desafiar(String nick) {
+        }
+
+        public void aceptarDesafio(Desafio d) {
+        }
+
+        public Ranking consultarRanking() {
+            return new Ranking();
+        }
+
+        public void registrarPersonaje() {
+        }
+
+        public void darDeBajaPersonaje() {
+            personaje = null;
+        }
+
+        public void elegirArmas() {
+        }
+
+        public void elegirArmaduras() {
+        }
+
+        public void rechazarDesafio() {
+        }
+
+        public void consultarOro() {
+        }
+
+        public void Operation1() {
+        }
+
+        // Getters y setters opcionales
+        public int getNumeroDeRegistro() {
+            return numeroDeRegistro;
+        }
+
+        public void setNumeroDeRegistro(int numeroDeRegistro) {
+            this.numeroDeRegistro = numeroDeRegistro;
+        }
+
+        public Personaje getPersonaje() {
+            return personaje;
+        }
+
+        public void setPersonaje(Personaje personaje) {
+            this.personaje = personaje;
+        }
+    }
+
+
