5edd7455a2c834f145f6262c6c75fed98f1e7101
“ivannpinna” <i.pina.2023@alumnos.urjc.es>
2025-04-25 18:06:15 +0200

    Hecho el paquete de equipment e implementado y hwcho tb user y empezado player

---
+++ src/equipment/Armor.java
@@ -0,0 +1,21 @@
+package equipment;
+
+import java.util.ArrayList;
+
+public class Armor extends Equipment{
+
+    public Armor(String name, int attackModifier, int defenseModifier) {
+        super(name, attackModifier, defenseModifier);
+    }
+
+    public static ArrayList<Armor> loadFromArray(String[][] armorsArray) {
+        ArrayList<Armor> armors = new ArrayList();
+
+        for(String[] armor : armorsArray) {
+            armors.add(new Armor(armor[0], Integer.parseInt(armor[1]), Integer.parseInt(armor[2])));
+        }
+
+        return armors;
+    }
+
+}
---
+++ src/equipment/Equipment.java
@@ -0,0 +1,41 @@
+package equipment;
+
+public abstract class Equipment {
+    private String name;
+    private int attack;
+    private int defense;
+
+    public Equipment(String name, int attack, int defense) {
+        this.name = name;
+        this.attack = attack;
+        this.defense = defense;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAttack(){
+        return this.attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public String toString() {
+        return this.name + " ( Ataque: " + this.attack + ", Defensa: " + this.defense + " )";
+    }
+}
