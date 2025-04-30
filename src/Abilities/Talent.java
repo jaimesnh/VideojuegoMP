920e8059e5c39e095b38a1062f7b7f1864a00106
“Turo” <arturo.vinuesadcg@gmail.com>
2025-04-30 12:49:13 +0200

    parte de abilities 2

--- src/Abilities/Talent.java
+++ src/Abilities/Talent.java
@@ -1,4 +1,34 @@
 package Abilities;
 
-public class Talent {
+import java.util.ArrayList;
+
+public class Talent extends SpecialAbility{
+    private int minimumRange;
+
+    public Talent (String name, int attack, int defense){
+        super(name, attack, defense);
+    }
+
+    public String toString() {
+        return super.toString() ;
+    }
+
+    public static String[] listTalents() {
+        String[] talents = new String[SystemGame.talentsAvailable.size()];
+        for (int i = 0; i < SystemGame.talentsAvailable.size(); i++) {
+            Talent talent = SystemGame.talentsAvailable.get(i);
+            talents[i] = talent.toString();
+        }
+
+        return talents;
+    }
+
+    public static ArrayList<Talent> loadFromArray(String[][] talentsList) {
+        ArrayList<Talent> talents = new ArrayList<>();
+        for (String[] tlnt : talentsList) {
+            talents.add(new Talent(tlnt[0], Integer.parseInt(tlnt[1]), Integer.parseInt(tlnt[2])));
+        }
+        return talents;
+    }
+
 }
