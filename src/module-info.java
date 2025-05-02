5efa9b50751dfc8159d5137a0f608e98f1fb9a4e
jaimesnh <jaimesv1203@gmail.com>
2025-05-02 18:05:28 +0200

    Arreglado el XMLAdapter para que funcione correctamente

--- src/module-info.java
+++ src/module-info.java
@@ -1,3 +1,13 @@
 module VideojuegoMP {
     requires java.desktop;
+
+    exports Abilities;
+    exports Challenges;
+    exports Character;
+    exports Equipment;
+    exports Main;
+    exports Minions;
+    exports SystemGame;
+    exports User;
+    exports Utils;
 }
